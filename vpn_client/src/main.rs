496d5ce5673a645fff6a398d176334572777f649
XicoBM <franciscomarques232@gmail.com>
2026-03-25 11:13:26 +0000

    Change the structure of the TUN Interface; remade it for use in windows using wintun

--- vpn_client/src/main.rs
+++ vpn_client/src/main.rs
@@ -1,45 +1,52 @@
-use std::{error::Error, io::Read, net::Ipv4Addr};
-use tun::create;
-use tun::Configuration;
-use tun::Device;
+use std::{error::Error, net::Ipv4Addr, sync::Arc};
+use wintun::{load, Adapter, Packet, Session};
 
 fn main() -> Result<(), Box<dyn Error + Send + Sync + 'static>> {
-    let mut tun0: Configuration = Configuration::default();
-
-    tun0.address((10, 0, 0, 9))
-        .netmask((255, 255, 255, 255)) // /32 for P2P
-        .destination((10, 0, 0, 1))
-        .mtu(1420)
-        .up();
-
-    let mut dev: Device = create(&tun0)?;
-    let mut buf: [u8; 1504] = [0; 1504];
+    let wintun = unsafe { load() }.expect("Wintun DDL not found");
+    let wintun_adapter: Arc<Adapter> =
+        Adapter::create(&wintun, "MyTun", "WireGuard", None).unwrap();
+    let session: Session = wintun_adapter
+        .start_session(wintun::MAX_RING_CAPACITY)
+        .unwrap();
+    let session: Arc<wintun::Session> = Arc::new(session);
 
     loop {
-        let n_bytes: usize = dev.read(&mut buf)?;
+        let packet: Packet = session.clone().receive_blocking()?;
+        let data: &[u8] = packet.bytes();
+        process_packet(data);
+    }
+}
 
-        if n_bytes >= 20 {
-            let origin: [u8; 4] = buf[12..16].try_into().unwrap();
-            let origin_ip: Ipv4Addr = Ipv4Addr::from_octets(origin);
+fn process_packet(data: &[u8]) {
+    if data.len() < 20 {
+        println!("Invalid packet! ({} bytes)", data.len());
+        return;
+    }
 
-            let destin: [u8; 4] = buf[16..20].try_into().unwrap();
-            let destin_ip: Ipv4Addr = Ipv4Addr::from_octets(destin);
+    let ip_version: u8 = (data[0] >> 4) & 0xF;
 
-            let protocol: u8 = buf[9];
+    if ip_version == 4 {
+        let origin_ip = Ipv4Addr::from(
+            <[u8; 4]>::try_from(&data[12..16]).expect("Couldn't reach the IP of origin"),
+        );
+        let destin_ip = Ipv4Addr::from(
+            <[u8; 4]>::try_from(&data[16..20]).expect("Couldn't reach the IP of destination"),
+        );
+        let protocol = data[9];
 
-            println!(
-                "The packet originates from {} directed to {} using [{}].",
-                origin_ip,
-                destin_ip,
-                match protocol {
-                    6 => "TCP",
-                    17 => "UDP",
-                    1 => "ICMP",
-                    _ => "Invalid Protocol",
-                },
-            )
-        } else {
-            println!("Invalid packet!");
-        }
+        println!(
+            "Packet: {} → {} [{}]",
+            origin_ip,
+            destin_ip,
+            match protocol {
+                1 => "ICMP",
+                6 => "TCP",
+                17 => "UDP",
+                p => {
+                    println!("Unknown protocol: {p}");
+                    return;
+                }
+            }
+        );
     }
 }
